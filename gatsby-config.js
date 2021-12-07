f3785fa18dcb9dd2c7c2a39ffdd1cc681872a64d
Blaze Dowis <blaze.dowis@wwex.com>
2021-12-07 13:12:30 -0800

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,22 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Janet Dowis",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `jpg`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
+    "gatsby-plugin-image",
+  ],
 };
